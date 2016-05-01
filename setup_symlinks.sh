f41ed494623b8e565bd43929d5aaa9e01330a61d
Noel Meaney <nmeaney.tech@gmail.com>
2016-05-02 00:16:37 +0100

    Update setup script

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -1,7 +1,19 @@
-#! usr/bin/env bash
+#!/bin/bash
 # Create symlinks from home directory
 
-echo "Create symlinks from your home directory to dotfiles"
-ln -s ./.bash_profile ~/.bash_profile
-ln -s ./.bash_aliases ~/.bash_aliases
-ln -s ./.app_dirs ~/.app_dirs
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "1) Backed up original .bash_profile and .bash_aliases"
+echo "2) Created symlinks from your home directory to $DIR"
+
+cp ~/.bash_profile ~/.bash_profile_backup
+rm ~/.bash_profile
+ln -s $DIR/.bash_profile ~/.bash_profile
+
+cp ~/.bash_aliases ~/.bash_aliases_backup
+rm ~/.bash_aliases
+ln -s $DIR/.bash_aliases ~/.bash_aliases
+
+rm ~/.app_dirs
+ln -s $DIR/.app_dirs ~/.app_dirs
+
