b0bd1174e45d8c10240a82d9290b17d578991723
Noel Meaney <nmeaney.tech@gmail.com>
2017-12-12 23:36:44 +0000

    Refactored a little

--- scripts/ncu-update-globals-v8.js
+++ scripts/ncu-update-globals-v8.js
@@ -3,18 +3,23 @@
 const util = require('util');
 const execFile = util.promisify(require('child_process').execFile);
 
-const packagesUpToDate = 'All global packages are up-to-date :)';
-
 async function getModulesToUpdate() {
 
   const { stdout: modulesToUpdate } = await execFile('ncu', ['-g']);
 
   console.log(modulesToUpdate);
 
+  return createUpdateCommands(modulesToUpdate);
+}
+
+function createUpdateCommands(modulesToUpdate) {
+
+  const packagesAreUpToDate = 'All global packages are up-to-date :)';
+
   return modulesToUpdate.split('\n')
     .filter(name => (name) ? name : false)
-    .filter(name => name !== packagesUpToDate)
-    .map(name => ['npm', ['install', '-g', name.split(' ')[1]]]);
+    .filter(name => name !== packagesAreUpToDate)
+    .map(name => ['npm', ['install', '-g', name.split(' ')[1]]]);  
 }
 
 async function updateModule(command) {
